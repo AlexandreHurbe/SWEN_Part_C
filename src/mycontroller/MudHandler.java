382d1e55484f8e3eec83d7dac88d75fd8c3e32ff
EthanW0411 <wlm454529340@gmail.com>
2018-05-22 23:01:27 +1000

    add inheritance to lavahandler and mudhandler

--- src/mycontroller/MudHandler.java
+++ src/mycontroller/MudHandler.java
@@ -1,5 +1,21 @@
 package mycontroller;
 
-public class MudHandler {
+import java.util.HashMap;
+
+import tiles.MapTile;
+import utilities.Coordinate;
+
+public class MudHandler extends TrapHandler{
+
+	public MudHandler(boolean blocked, HashMap<Coordinate, MapTile> view) {
+		super(blocked, view);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public boolean handleTrap(Path path) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 }
