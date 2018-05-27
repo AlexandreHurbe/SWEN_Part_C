82ab87de278b0ebd2adccde9caafd880924f3815
bowenade <lbwde96@gmail.com>
2018-05-27 21:18:39 +1000

    better checking walls

--- src/mycontroller/WallHandler.java
+++ src/mycontroller/WallHandler.java
@@ -9,16 +9,18 @@ public class WallHandler extends Handler{
 	public void handleMapTile() {
 		
 	}
-	
+	// check the tile is next to walls
 	public int handleMapTile(Coordinate end) {
 		int wall = 0;
 		Coordinate current;
 		for(int i = -1; i < 1; i++) {
 			for (int j = -1; j < 1 ; j++) {
 				current = new Coordinate(end.x+i, end.y+j);
-				if(myMap.getMap().get(current) != null) {
-					if(myMap.getMap().get(current).isType(Type.WALL)) {
-						wall +=1;
+				if(i != j) {
+					if(myMap.getMap().get(current) != null) {
+						if(myMap.getMap().get(current).isType(Type.WALL)) {
+							wall +=1;
+						}
 					}
 				}
 			}
